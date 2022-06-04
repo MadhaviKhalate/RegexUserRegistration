9b5450ba9a3cfedbcb92471e6aa08d0c40eff25a
MadhaviKhalate <khalatemadhavi1610@gmail.com>
2022-06-04 09:37:16 +0530

    [Madhavi] First name validation

---
+++ RegexUserRegistration/RegexUserRegistration/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace RegexUserRegistration
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Enter first name");
+            string fname = Console.ReadLine();
+            RegExp regex = new RegExp();
+            regex.RegexFirstName(fname);
+        }
+    }
+}
