260f3a5216eb5a166d328b1c8fd9d1b4f39d1192
MadhaviKhalate <khalatemadhavi1610@gmail.com>
2022-06-04 11:03:21 +0530

    [Madhavi] Last name validation

--- RegexUserRegistration/RegexUserRegistration/Program.cs
+++ RegexUserRegistration/RegexUserRegistration/Program.cs
@@ -6,10 +6,27 @@ namespace RegexUserRegistration
     {
         public static void Main(string[] args)
         {
-            Console.WriteLine("Enter first name");
-            string fname = Console.ReadLine();
+            Console.WriteLine("1.Validate First Name \n2.Validate Last Name \n ");
+            Console.WriteLine("Enter your option");
+            int option = Convert.ToInt32(Console.ReadLine());
             RegExp regex = new RegExp();
-            regex.RegexFirstName(fname);
+
+            switch (option)
+            {
+                case 1:
+                    Console.WriteLine("Enter first name");
+                    string fname = Console.ReadLine();
+                    regex.RegexFirstName(fname);
+                    break;
+
+                case 2:
+                    Console.WriteLine("Enter last name");
+                    string lname = Console.ReadLine();
+                    regex.RegexLastName(lname);
+                    break;
+
+            }
+            
         }
     }
 }
--- RegexUserRegistration/RegexUserRegistration/RegExp.cs
+++ RegexUserRegistration/RegexUserRegistration/RegExp.cs
@@ -10,7 +10,7 @@ namespace RegexUserRegistration
     public class RegExp
     {
         const string fname = "^[A-Z]{1}[a-z]{2,}$";
-
+        const string lname = "^[A-Z]{1}[a-z]{2,}$";
         public void RegexFirstName(string firstName)
         {
             if (Regex.IsMatch(firstName, fname))
@@ -19,6 +19,16 @@ namespace RegexUserRegistration
                 return;
             }
             Console.WriteLine("First name is not valid");
+        }
+
+        public void RegexLastName(string lastName)
+        {
+            if (Regex.IsMatch(lastName, lname))
+            {
+                Console.WriteLine("Last name is valid");
+                return;
+            }
+            Console.WriteLine("Last name is not valid");
         }
     }
 }
